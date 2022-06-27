6923bc8aebb0e8ee0f8e856e66b14b01667de7f5
Troy Kessler <troy.kessler99@gmail.com>
2022-06-27 12:20:10 +0200

    increased version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import KYVE from '@kyve/core';
+import KYVE, { Item } from '@kyve/core';
 import { Signature } from './types';
 import { fetchBlock, fetchHeight, isBlockNotFound } from './utils';
 import { name, version } from '../package.json';
@@ -11,32 +11,42 @@ KYVE.metrics.register.setDefaultLabels({
 });
 
 class KyveNear extends KYVE {
-  public async getDataItem(key: number): Promise<{ key: number; value: any }> {
+  public async getDataItem(key: string): Promise<Item> {
     let block;
 
     const height = await fetchHeight(
       this.pool.config.rpc,
       await this.getSignature()
     );
-    if (key > height) throw new Error();
+    if (+key > height) throw new Error();
 
     try {
       block = await fetchBlock(
         this.pool.config.rpc,
-        key,
+        +key,
         await this.getSignature()
       );
     } catch (err) {
       if (isBlockNotFound(err)) return { key, value: null };
 
-      this.logger.warn(
-        `⚠️  EXTERNAL ERROR: Failed to fetch block ${key}. Retrying ...`
-      );
+      this.logger.warn(`Failed to fetch block ${key}. Retrying ...`);
 
       throw err;
     }
 
     return { key, value: block };
+  }
+
+  public async getNextKey(key: string): Promise<string> {
+    if (key) {
+      return (parseInt(key) + 1).toString();
+    }
+
+    return '0';
+  }
+
+  public async formatValue(value: any): Promise<string> {
+    return value.header?.hash ?? 'error';
   }
 
   private async getSignature(): Promise<Signature> {
